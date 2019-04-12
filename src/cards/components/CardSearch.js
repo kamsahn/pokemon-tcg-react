4ea37708c6d18d2dcc1a8ace157188d3d019090f
kamsahn <samjkahn@gmail.com>
2019-04-11 22:15:13 -0400

    Make drop down select in CardSearch
    - can only choose from applicable sets
    - automatically calls set code and totalCards to state
    - used to display cards when necessary

--- src/cards/components/CardSearch.js
+++ src/cards/components/CardSearch.js
@@ -12,37 +12,59 @@ class CardSearch extends Component {
 
     this.state = {
       cards: [],
-      set: null
+      setCode: null,
+      setTotalCards: null
     }
   }
 
   handleChange = event => {
-    this.setState({ set: event.target.value })
+    this.setState({
+      setCode: event.target.value,
+      setTotalCards: store.sets.find(set => set.code === event.target.value).totalCards
+    })
   }
 
   clickSearch = (event) => {
     event.preventDefault()
-    const { set } = this.state
-    const totalCards = store.sets.find(selected => selected.code === set).totalCards
+    const { setCode, setTotalCards } = this.state
     store.cards = []
-    getCards(set)
+    getCards(setCode)
       .on('data', response => {
         store.cards.push(response)
-        if (store.cards.length === totalCards) {
+        if (store.cards.length === setTotalCards) {
           this.setState({ cards: sortedCards(store.cards) })
         }
       })
   }
 
   render () {
-    const { cards } = this.state
+    const { cards, setTotalCards } = this.state
     return (
       <Fragment>
         <form onSubmit={this.clickSearch}>
-          <input name="set" type="text" onChange={this.handleChange}/>
+          <select onChange={this.handleChange} id="set-select" name="set-select">
+            <option value="">--Choose a set--</option>
+            <option value="base1">Base</option>
+            <option value="base2">Jungle</option>
+            <option value="basep">Wizards Black Star Promos</option>
+            <option value="base3">Fossil</option>
+            <option value="base4">Base Set 2</option>
+            <option value="base5">Team Rocket</option>
+            <option value="gym1">Gym Heros</option>
+            <option value="gym2">Gym Challenge</option>
+            <option value="neo1">Neo Genesis</option>
+            <option value="neo2">Neo Discovery</option>
+            <option value="si1">Southern Islands</option>
+            <option value="neo3">Neo Revolution</option>
+            <option value="neo4">Neo Destiny</option>
+            <option value="base6">Legendary Collection</option>
+            <option value="ecard1">Expedition Base Set</option>
+            <option value="ecard2">Aquapolis</option>
+            <option value="ecard3">Skyridge</option>
+          </select>
           <input type="submit" className="btn btn-info space" value="Get Cards"/>
         </form>
-        {cards && cards.length === 102 ? (
+        {cards && cards.length === setTotalCards ? (
           cards.map(card => (
             <Card
               key={card.id}
