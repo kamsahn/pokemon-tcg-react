5df0e509adb75332bbcc0d00181a08718ec07b83
kamsahn <samjkahn@gmail.com>
2019-04-15 16:30:01 -0400

    Add error message about 4 of a kind

--- src/cards/messages.js
+++ src/cards/messages.js
@@ -2,7 +2,7 @@ export default {
   searchByNameFailure: 'Could not find the card you specified. Make sure you spelt it correctly or try searching by set.',
   searchBySetFailure: 'Could not find the cards of this set. There may be an issue with the card api, check back later.',
   cardCreateSuccess: 'Successfully added this card to your deck.',
-  cardCreateFailure: 'Could not add your card. Note: you can only have 60 cards in a deck.',
+  cardCreateFailure: 'Could not add your card. Note: you can only have 60 cards in a deck and only 4 of a kind (excluding basic energies).',
   cardDeleteSuccess: 'Successfully deleted this card from your deck.',
   cardDeleteFailure: 'Could not delete this card. Make sure you are signed in correctly.'
 }
