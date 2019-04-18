b01a67e5a894cc5a3c7c5e19e7685a46acbd8493
kamsahn <samjkahn@gmail.com>
2019-04-18 12:46:10 -0400

    Edit error messages

--- src/auth/messages.js
+++ src/auth/messages.js
@@ -1,6 +1,6 @@
 export default {
   signUpSuccess: 'Succesfully registered! You\'ve been signed in as well.',
-  signUpFailure: 'Registration failed. Email may be taked, or passwords' +
+  signUpFailure: 'Registration failed. Email may be taken, or passwords' +
    ' don\'t match.',
   signInSuccess: 'Welcome!',
   signInFailure: 'Failed to sign in. Check your email and password and try' +
